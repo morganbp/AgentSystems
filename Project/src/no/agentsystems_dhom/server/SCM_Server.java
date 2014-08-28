03b500d49948e2f480922e4087a14f1d04819d0a
davidhel <dav-h@hotmail.com>
2014-08-28 11:47:08 +0200

    merge rett

--- Project/src/no/agentsystems_dhom/server/SCM_Server.java
+++ Project/src/no/agentsystems_dhom/server/SCM_Server.java
@@ -1,8 +1,5 @@
 package no.agentsystems_dhom.server;
 
-<<<<<<< HEAD
-public class SCM_Server {
-=======
 
 public class SCM_Server extends Thread{
 	private SCM server;
@@ -47,6 +44,5 @@ public class SCM_Server extends Thread{
 	public Message agentRegistering(Message kqml){
 		//TODO register agent
 	}
->>>>>>> 17f950db827cd0bfd13506ce3e2e64bc7855270d
 	
 }
