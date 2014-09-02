bfb21b5e5fd15efe35285835d3a8934383c9b1d6
athey <hansgudim@gmail.com>
2014-09-02 10:39:40 +0200

    Lagt til SCM_S1 og SCM_Suppler

---
+++ Project/src/no/agentsystem_dhom/supplier/SCM_S1.java
@@ -0,0 +1,20 @@
+package no.agentsystem_dhom.supplier;
+
+import TACSCMApp.SCM;
+
+public class SCM_S1 {
+	public SCM_S1(SCM server)
+	{
+		
+	}
+	
+	public void run()
+	{
+		
+	}
+	
+	public void main()
+	{
+		
+	}
+}
---
+++ Project/src/no/agentsystem_dhom/supplier/SCM_Supplier.java
@@ -0,0 +1,27 @@
+package no.agentsystem_dhom.supplier;
+
+import no.agentsystems_dhom.server.GUI;
+import no.agentsystems_dhom.server.SCM_Server;
+
+public class SCM_Supplier {
+	private SCM_Server server;
+	private int interval;
+	boolean has_started;
+	GUI gui;
+	
+	public void startTheGame()
+	{
+		
+	}
+	
+	public void closeTheGame()
+	{
+		
+	}
+	
+	public boolean getStatus()
+	{
+		return true;
+	}
+
+}
