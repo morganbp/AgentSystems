da633a56db19619d67a166ff94e6ac70f3ea821a
morganbp <morgibp@gmail.com>
2014-09-04 22:38:26 +0200

    Fikset litt i customer client delen.

--- Project/src/no/agentsystems_dhom/customer/SCM_C1.java
+++ Project/src/no/agentsystems_dhom/customer/SCM_C1.java
@@ -1,16 +1,48 @@
 package no.agentsystems_dhom.customer;
 
-public class SCM_C1 extends SCM_Customer{
-	
-	public SCM_C1(SCM ser){
-		
+import no.agentsystems_dhom.game_elements.GUI;
+import no.agentsystems_dhom.game_elements.TAC_Ontology;
+
+public class SCM_C1 extends SCM_Customer {
+
+	private int interval;
+	private int time;
+	private int day;
+
+	public SCM_C1() {
+		interval = scmImpl.getTime();
+		GUI custView = new GUI("SCM_C1");
+		try {
+			custView.setText("SCM_C1 is connected!");
+
+			for (;;) {
+
+				if (scmImpl.status() && !getStatus()) {
+					startTheGame();
+				}else if(interval == TAC_Ontology.gameLength){
+					closeTheGame();
+				}
+				
+
+				time = interval % TAC_Ontology.lengthOfADay;
+
+				if (time == 0 && getStatus()) {
+					day = interval / TAC_Ontology.lengthOfADay;
+					custView.append("\nday : " + day);
+				}
+
+				interval++;
+
+				Thread.sleep(TAC_Ontology.sec);
+			}
+		} catch (InterruptedException e) {
+
+		}
 	}
-	
-	public static void main(String[] args){
-		
+
+	public static void main(String[] args) {
+		initSCMImpl(args);
+		new SCM_C1();
 	}
-	
-	public void run(){
-		
-	}
+
 }
--- Project/src/no/agentsystems_dhom/customer/SCM_Customer.java
+++ Project/src/no/agentsystems_dhom/customer/SCM_Customer.java
@@ -11,13 +11,11 @@ import TACSCMApp.SCMHelper;
 
 
 public class SCM_Customer {
-	private SCM server;
-	private int interval;
+	
 	private boolean has_started;
-	static GUI custgui;
 	static TACSCMApp.SCM scmImpl;
 	
-	public static void main(String[] args){
+	public static void initSCMImpl(String[] args){
 		try{
 			// create and initialize the ORB
 	        ORB orb = ORB.init(args, null);
@@ -32,24 +30,7 @@ public class SCM_Customer {
 	        // resolve the Object Reference in Naming
 	        String name = "TACSCM";
 	        scmImpl = SCMHelper.narrow(ncRef.resolve_str(name));
-	        custgui = new GUI("SCM_C1");
-
-        	short curTime = scmImpl.getTime();
-        	custgui.setText("---->> Time: "+Short.toString(curTime) + " seconds");
-        	
-	        for(;;){
-	        	
-	        	System.out.println();
-	        	if(curTime % 9 == 0){
-	        		custgui.append("\nDay: " + Integer.toString(curTime/9));
-	        	}
-	        	
-	        	
-	        	Thread.sleep(TAC_Ontology.sec);
-	        }
-		}catch(InterruptedException e){
-			System.out.println("ERROR : " + e);
-			e.printStackTrace();
+	
 		}catch(Exception e){
 			System.out.println("ERROR : " + e);
 			e.printStackTrace();
@@ -57,10 +38,12 @@ public class SCM_Customer {
 	}
 	
 	protected void startTheGame(){
+		has_started = true;
 		
 	}
 	
 	protected void closeTheGame(){
+		has_started = false;
 		
 	}
 	
