eef17e145ea04d932304a45a3d186525424b06de
morganbp <morgibp@gmail.com>
2014-09-04 11:19:56 +0200

    Implementerte kode i SCM_Customer så den nå kan kommunisere med serveren.

--- Project/src/no/agentsystems_dhom/customer/SCM_Customer.java
+++ Project/src/no/agentsystems_dhom/customer/SCM_Customer.java
@@ -1,10 +1,55 @@
 package no.agentsystems_dhom.customer;
 
+import no.agentsystems_dhom.game_elements.GUI;
+import no.agentsystems_dhom.game_elements.TAC_Ontology;
+
+import org.omg.CORBA.ORB;
+import org.omg.CosNaming.NamingContextExt;
+import org.omg.CosNaming.NamingContextExtHelper;
+
+import TACSCMApp.SCMHelper;
+
 
 public class SCM_Customer {
 	private SCM server;
 	private int interval;
-	private boolean has_started;	
+	private boolean has_started;
+	static GUI custgui;
+	static TACSCMApp.SCM scmImpl;
+	
+	public static void main(String[] args){
+		try{
+			// create and initialize the ORB
+	        ORB orb = ORB.init(args, null);
+
+	        // get the root naming context
+	        org.omg.CORBA.Object objRef = 
+	            orb.resolve_initial_references("NameService");
+	        // Use NamingContextExt instead of NamingContext. This is 
+	        // part of the naming Service.  
+	        NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+					
+	        // resolve the Object Reference in Naming
+	        String name = "TACSCM";
+	        scmImpl = SCMHelper.narrow(ncRef.resolve_str(name));
+	        custgui = new GUI("SCM_C1");
+	        for(;;){
+	        	
+	        	short curTime = scmImpl.getTime();
+	        	custgui.setText("---->> Time: "+Short.toString(curTime) + " seconds");
+	        	
+	        	
+	        	
+	        	Thread.sleep(TAC_Ontology.sec);
+	        }
+		}catch(InterruptedException e){
+			System.out.println("ERROR : " + e);
+			e.printStackTrace();
+		}catch(Exception e){
+			System.out.println("ERROR : " + e);
+			e.printStackTrace();
+		}
+	}
 	
 	protected void startTheGame(){
 		
