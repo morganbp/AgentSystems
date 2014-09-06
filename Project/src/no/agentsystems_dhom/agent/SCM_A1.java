5690c04aaad6d93c933d87ed1a6a62ef7d680f31
davidhel <dav-h@hotmail.com>
2014-09-06 14:36:52 +0200

    Lagd SCM_AGENT

--- Project/src/no/agentsystems_dhom/agent/SCM_A1.java
+++ Project/src/no/agentsystems_dhom/agent/SCM_A1.java
@@ -1,5 +1,13 @@
 package no.agentsystems_dhom.agent;
 
 public class SCM_A1 extends SCM_Agent{
+	public SCM_A1(){
+		interval = scmImpl.getTime();
+		
+	}
+	public static void main(String[] args){
+		initSCMImpl(args);
+		new SCM_A1();
+	}
 
 }
--- Project/src/no/agentsystems_dhom/agent/SCM_Agent.java
+++ Project/src/no/agentsystems_dhom/agent/SCM_Agent.java
@@ -1,19 +1,53 @@
 package no.agentsystems_dhom.agent;
 
+import org.omg.CORBA.ORB;
+import org.omg.CosNaming.NamingContextExt;
+import org.omg.CosNaming.NamingContextExtHelper;
+
 import no.agentsystems_dhom.game_elements.GUI;
 import TACSCMApp.SCM;
+import TACSCMApp.SCMHelper;
 
 public class SCM_Agent {
 	private SCM server;
-	private int interval;
+	protected int interval;
 	private boolean has_started;
-	private GUI gui;
+	private GUI agentView;
+	static TACSCMApp.SCM scmImpl;
+	
+	public static void initSCMImpl(String[] args){
+		try{
+
+			// create and initialize the ORB
+	        ORB orb = ORB.init(args, null);
+
+	        // get the root naming context
+	        org.omg.CORBA.Object objRef = 
+	            orb.resolve_initial_references("NameService");
+	        // Use NamingContextExt instead of NamingContext. This is 
+	        // part of the naming Service.  
+	        NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+					
+	        // resolve the Object Reference in Naming
+	        String name = "TACSCM";
+	        scmImpl = SCMHelper.narrow(ncRef.resolve_str(name));
+			
+		}
+		catch(Exception e){
+			System.out.println("ERROR :" + e);
+			e.printStackTrace();
+		}
+	}
 	
 	protected void startTheGame(){
+		has_started = true;
+		interval = 0;
+		agentView.append("---> TAC Game started.");
 		
 	}
 	protected void closeTheGame(){
-		
+		has_started = false;
+		agentView.append("\n\n---> The TAC Game is closed");
 	}
 	protected boolean getStatus(){
 		return has_started;
