2f758ad3c2b02cb5136cdee9a7f02793ad7f84e6
Hans-Ole Gudim <hansgudim@gmail.com>
2014-09-15 22:10:20 +0200

    Begynt på sendDailyRFQs og laget RFQ-klasse.

--- Project/src/no/agentsystems_dhom/customer/SCM_C1.java
+++ Project/src/no/agentsystems_dhom/customer/SCM_C1.java
@@ -1,8 +1,14 @@
 package no.agentsystems_dhom.customer;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
 import no.agentsystems_dhom.game_elements.GUI;
 import no.agentsystems_dhom.game_elements.TAC_Ontology;
 import TACSCMApp.SCM;
+import no.agentsystems_dhom.game_elements.RFQ;;
 
 public class SCM_C1 extends SCM_Customer {
 
@@ -29,6 +35,7 @@ public class SCM_C1 extends SCM_Customer {
 				if (time == 0 && getStatus()) {
 					int day = interval / TAC_Ontology.lengthOfADay;	
 					custView.append("\nday: " + day);
+					//Send RFQs to server
 				}
 				interval++;
 
@@ -42,6 +49,28 @@ public class SCM_C1 extends SCM_Customer {
 	public static void main(String[] args) {
 		SCM ser = initSCMImpl(args);
 		new SCM_C1(ser);
+	}
+	
+	public void sendDailyRFQs(int RFQQuantity, int currentDay)
+	{
+		Random rand = new Random();
+		List<RFQ> RFQs = new ArrayList<RFQ>();
+		
+		//Low Segment
+		for(int i = 0; i < RFQQuantity; i++)
+		{
+			//NOT FINISHED HERE
+			int reservePrice = rand.nextInt(TAC_Ontology.PCpmax - TAC_Ontology.PCpmin)+ TAC_Ontology.PCpmin;
+			int penalty = rand.nextInt(TAC_Ontology.Pmax - TAC_Ontology.Pmin) + TAC_Ontology.Pmin;
+			
+			int PC = 1;
+			int quantity = rand.nextInt(TAC_Ontology.Qmax - TAC_Ontology.Qmin) + TAC_Ontology.Qmin;
+			int dueDate = currentDay + rand.nextInt(TAC_Ontology.Dmax - TAC_Ontology.Dmin) + TAC_Ontology.Dmin;
+			RFQ tempRFQ = new RFQ(PC, quantity, dueDate, penalty, reservePrice);
+			RFQs.add(tempRFQ);
+		}
+		
+		
 	}
 
 }
---
+++ Project/src/no/agentsystems_dhom/game_elements/RFQ.java
@@ -0,0 +1,27 @@
+package no.agentsystems_dhom.game_elements;
+
+public class RFQ {
+
+	static int nextId = 1;
+	int RFQId;
+	int PC;
+	int quantity;
+	int dueDate;
+	int penalty;
+	int reservePrice;
+	
+	public RFQ(int PC, int quantity, int dueDate, int penalty, int reservePrice)
+	{
+		//Setting a unique ID for the RFQ
+		this.RFQId = nextId;
+		nextId++;
+		
+		this.PC = PC;
+		this.quantity = quantity;
+		this.dueDate = dueDate;
+		this.penalty = penalty;
+		this.reservePrice = reservePrice;
+	}
+	
+}
+
