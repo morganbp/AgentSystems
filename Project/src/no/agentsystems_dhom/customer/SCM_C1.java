32e4933fca537389e65a9aeccf9e71a8179a7a74
athey <hansgudim@gmail.com>
2014-09-17 11:37:18 +0200

    Moved the sendRFQ-code form SCM_C1 to SCM_Customer so all agents will inherit it. Completed the logic of creating a random number of RFQs, but stil missing code to submit the RFQs to the server.

--- Project/src/no/agentsystems_dhom/customer/SCM_C1.java
+++ Project/src/no/agentsystems_dhom/customer/SCM_C1.java
@@ -52,47 +52,6 @@ public class SCM_C1 extends SCM_Customer {
 	}
 	
 	
-	//W.I.P. 
-	public void sendDailyRFQs(int RFQQuantity, int currentDay)
-	{
-		Random rand = new Random();
-		List<RFQ> RFQs = new ArrayList<RFQ>();
-		
-		//Decide the number of RFQs to send to the server
-		double trend = 1;
-		trend = Math.max(TAC_Ontology.Tmin,  Math.min(TAC_Ontology.Tmax, trend + (0.02*rand.nextDouble() - 0.01)));
-		double RFQavg = TAC_Ontology.HLRFQmin + (TAC_Ontology.HLRFQmax - TAC_Ontology.HLRFQmin)* rand.nextDouble(); 
-		//Setting trend to 1 if it exceeds the boundaries
-		if(trend*RFQavg < TAC_Ontology.HLRFQmin || trend*RFQavg > TAC_Ontology.HLRFQmax)
-		{
-			trend = 1;
-		}
-		
-		//Compute the number of RFQs for the current day for each segment(high, low, mid)
-		//Low and high segment
-		double HLRFQavg = Math.min(TAC_Ontology.HLRFQmax,  Math.max(TAC_Ontology.HLRFQmin, RFQavg)*trend); 
-		//DO POISSON
-		
-		
-		//Mid segment
-		double MRFQavg = Math.min(TAC_Ontology.MRFQmax, Math.max(TAC_Ontology.MRFQmin, RFQavg)*trend);
-		//DO POISSON
-		
-		//Add RFQs with random content to the list
-		for(int i = 0; i < RFQQuantity; i++)
-		{
-			//NOT FINISHED HERE
-			int reservePrice = rand.nextInt(TAC_Ontology.PCpmax - TAC_Ontology.PCpmin)+ TAC_Ontology.PCpmin;
-			int penalty = rand.nextInt(TAC_Ontology.Pmax - TAC_Ontology.Pmin) + TAC_Ontology.Pmin;
-			
-			int PC = 1;
-			int quantity = rand.nextInt(TAC_Ontology.Qmax - TAC_Ontology.Qmin) + TAC_Ontology.Qmin;
-			int dueDate = currentDay + rand.nextInt(TAC_Ontology.Dmax - TAC_Ontology.Dmin) + TAC_Ontology.Dmin;
-			RFQ tempRFQ = new RFQ(PC, quantity, dueDate, penalty, reservePrice);
-			RFQs.add(tempRFQ);
-		}
-		
-		
-	}
+	
 
 }
--- Project/src/no/agentsystems_dhom/customer/SCM_Customer.java
+++ Project/src/no/agentsystems_dhom/customer/SCM_Customer.java
@@ -1,6 +1,11 @@
 package no.agentsystems_dhom.customer;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import no.agentsystems_dhom.game_elements.GUI;
+import no.agentsystems_dhom.game_elements.RFQ;
 import no.agentsystems_dhom.game_elements.TAC_Ontology;
 
 import org.omg.CORBA.ORB;
@@ -10,59 +15,140 @@ import org.omg.CosNaming.NamingContextExtHelper;
 import TACSCMApp.SCM;
 import TACSCMApp.SCMHelper;
 
-
 public class SCM_Customer {
-	
+
 	private boolean has_started;
 	protected SCM server;
-	protected  GUI custView;
+	protected GUI custView;
 	protected int interval;
-	
-	public static SCM initSCMImpl(String[] args){
-		SCM server = null;
-		try{
-			// create and initialize the ORB
-	        ORB orb = ORB.init(args, null);
 
-	        // get the root naming context
-	        org.omg.CORBA.Object objRef = 
-	            orb.resolve_initial_references("NameService");
-	        // Use NamingContextExt instead of NamingContext. This is 
-	        // part of the naming Service.  
-	        NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
-					
-	        // resolve the Object Reference in Naming
-	        String name = "TACSCM";
-	        server = SCMHelper.narrow(ncRef.resolve_str(name));
-	
-		}catch(Exception e){
+	public static SCM initSCMImpl(String[] args) {
+		SCM server = null;
+		try {
+			// create and initialize the ORB
+			ORB orb = ORB.init(args, null);
+
+			// get the root naming context
+			org.omg.CORBA.Object objRef = orb
+					.resolve_initial_references("NameService");
+			// Use NamingContextExt instead of NamingContext. This is
+			// part of the naming Service.
+			NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+
+			// resolve the Object Reference in Naming
+			String name = "TACSCM";
+			server = SCMHelper.narrow(ncRef.resolve_str(name));
+
+		} catch (Exception e) {
 			System.out.println("ERROR : " + e);
 			e.printStackTrace();
 			return null;
 		}
 		return server;
 	}
-	
-	protected void startTheGame(){
+
+	protected void startTheGame() {
 		has_started = true;
-		interval = (interval < 0 || interval > TAC_Ontology.gameLength) ? 0 : interval;
+		interval = (interval < 0 || interval > TAC_Ontology.gameLength) ? 0
+				: interval;
 		custView.setText("---> Time : " + interval + " seconds ");
 	}
-	
-	protected void closeTheGame(){
+
+	protected void closeTheGame() {
 		has_started = false;
-		
+
 		custView.append("\n\n---> The TAC Game is closed");
-		
+
 	}
-	
-	protected boolean getStatus(){
+
+	protected boolean getStatus() {
 		return has_started;
 	}
-	
-	protected void setStatus(boolean status){
+
+	protected void setStatus(boolean status) {
 		has_started = status;
 	}
-	
+
+	// W.I.P.
+	protected void sendDailyRFQs(int currentDay, String className) {
+		Random rand = new Random();
+		List<RFQ> RFQs = new ArrayList<RFQ>();
+
+		// Decide the number of RFQs to send to the server
+		double trend = 1;
+		trend = Math.max(
+				TAC_Ontology.Tmin,
+				Math.min(TAC_Ontology.Tmax, trend
+						+ (0.02 * rand.nextDouble() - 0.01)));
+		double RFQavg = TAC_Ontology.HLRFQmin
+				+ (TAC_Ontology.HLRFQmax - TAC_Ontology.HLRFQmin)
+				* rand.nextDouble();
+		// Setting trend to 1 if it exceeds the boundaries
+		if (trend * RFQavg < TAC_Ontology.HLRFQmin || trend * RFQavg > TAC_Ontology.HLRFQmax) {
+			trend = 1;
+		}
+
+		// Compute the number of RFQs for the current day for each segment
+		// High segment
+		double HRFQavg = Math.min(TAC_Ontology.HLRFQmax,
+		Math.max(TAC_Ontology.HLRFQmin, RFQavg) * trend);
+		List<RFQ> highSegmentRFQs = createRFQs(HRFQavg, TAC_Ontology.high, currentDay);
+		// DO POISSON?
+
+		// Low segment
+		double LRFQavg = Math.min(TAC_Ontology.HLRFQmax,
+		Math.max(TAC_Ontology.HLRFQmin, RFQavg) * trend);
+		List<RFQ> lowSegmentRFQs = createRFQs(LRFQavg, TAC_Ontology.low, currentDay);
+		// DO POISSON?
+		
+		// Mid segment
+		double MRFQavg = Math.min(TAC_Ontology.MRFQmax,
+		Math.max(TAC_Ontology.MRFQmin, RFQavg) * trend);
+		List<RFQ> midSegmentRFQs = createRFQs(MRFQavg, TAC_Ontology.mid, currentDay);
+		// DO POISSON?
+
+		//Adding RFQs from each segment to the main list
+		RFQs.addAll(lowSegmentRFQs);
+		RFQs.addAll(midSegmentRFQs);
+		RFQs.addAll(highSegmentRFQs);
+		
+		//Logic to send the RFQs to the server
+		// IMPLEMENT THIS
+		//server.send(RFQs, className)
+		
+	}
+
+	//create RFQ based on segment, what day it is and how many we want to create.
+	protected List<RFQ> createRFQs(double RFQquantity, int segment, int currentDay) {
+		Random rand = new Random();
+		List<RFQ> RFQs = new ArrayList<RFQ>();
+		
+		for (int i = 0; i < RFQquantity; i++) {
+			//Get SKU and create PC in chosen segment
+			int SKU =  PC.SKU(segment);
+			PC pc = new PC(SKU);
+			
+			// NOT FINISHED HERE
+			int reservePrice = pc.getbasePrice()*((rand.nextInt(TAC_Ontology.PCpmax
+					- TAC_Ontology.PCpmin)
+					+ TAC_Ontology.PCpmin)/100);
+			
+			int penalty = (rand.nextInt(TAC_Ontology.Pmax - TAC_Ontology.Pmin)
+					+ TAC_Ontology.Pmin) * reservePrice/100;
+
+			int PCsku = pc.getSKU();
+			
+			int quantity = rand.nextInt(TAC_Ontology.Qmax - TAC_Ontology.Qmin)
+					+ TAC_Ontology.Qmin;
+			
+			int dueDate = currentDay
+					+ rand.nextInt(TAC_Ontology.Dmax - TAC_Ontology.Dmin)
+					+ TAC_Ontology.Dmin;
+			
+			RFQ tempRFQ = new RFQ(PCsku, quantity, dueDate, penalty, reservePrice);
+			RFQs.add(tempRFQ);
+		}
+		return RFQs;
+	}
 
 }
