6a7713c97338bfc07914e3bbcc110c37f3e4e7e9
morganbp <morgibp@gmail.com>
2014-09-19 12:58:15 +0200

    Ferdig med Offer klassen. La til methodene stringToList og listToString

--- Project/src/no/agentsystems_dhom/server/Offer.java
+++ Project/src/no/agentsystems_dhom/server/Offer.java
@@ -1,5 +1,6 @@
 package no.agentsystems_dhom.server;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Offer {
@@ -41,7 +42,39 @@ public class Offer {
 				+ OFFER_FIELD_SEPERATOR + rfq.toString();
 	}
 	
-	public static List<Offer> stringToList(String offerString){
-		return null;
+	public static String listToString(List<Offer> offersList){
+		StringBuilder builder = new StringBuilder();
+		boolean firstrun = true;
+		for(Offer offer : offersList){
+			if(!firstrun)
+				builder.append(OFFER_OBJECT_SEPERATOR);
+			else
+				firstrun = false;
+			
+			builder.append(offer.toString());
+		}
+		return builder.toString();
 	}
+	
+	public static List<Offer> stringToList(String offersString){
+		List<Offer> rtnList = new ArrayList<>();
+		String[] offers = offersString.split(OFFER_OBJECT_SEPERATOR);
+		for(String offer : offers ){
+			Offer o = toOffer(offer);
+			if(o == null) continue;
+			rtnList.add(o);
+		}
+		return rtnList;
+	}
+	
+	public static Offer toOffer(String offer){
+		String[] offFields = offer.split(OFFER_FIELD_SEPERATOR);
+		if(offFields.length != 4) return null;
+		
+		RFQ rfq = RFQ.toRFQ(offFields[3]);
+		if(rfq == null) return null;
+		
+		return new Offer(offFields[0], offFields[1], Double.parseDouble(offFields[2]), rfq);
+	}
+	
 }
