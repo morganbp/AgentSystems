e17a6fff1b45c508c0f196d32dd4f94d2116dced
davidhel <dav-h@hotmail.com>
2014-10-30 11:35:20 +0100

    Added Assembly in Agent

--- Project/src/no/agentsystems_dhom/agent/Assembly.java
+++ Project/src/no/agentsystems_dhom/agent/Assembly.java
@@ -1,5 +1,18 @@
 package no.agentsystems_dhom.agent;
 
+import no.agentsystems_dhom.server.TAC_Ontology;
+
 public class Assembly {
+	private int numberOfCycles = 0;
+	private int cycles[] = {4,5,5,6,5,6,6,7,4,5,5,6,5,5,6,7};
+	public Assembly(){
+		numberOfCycles = TAC_Ontology.agentCellCapacity;
+	}
+	public void updateCycle(int sku, int n) {
+		numberOfCycles -= n * cycles[sku-1];
+	}
+	public void resetAssenbly() {
+		numberOfCycles = TAC_Ontology.agentCellCapacity;
+  	}
 
 }
