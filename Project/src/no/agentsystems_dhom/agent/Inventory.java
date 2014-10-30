c4c9a06b305d77fb215b2637abbab28a37da325d
davidhel <dav-h@hotmail.com>
2014-10-30 11:24:45 +0100

    Added Inventory to Agent

--- Project/src/no/agentsystems_dhom/agent/Inventory.java
+++ Project/src/no/agentsystems_dhom/agent/Inventory.java
@@ -1,5 +1,13 @@
 package no.agentsystems_dhom.agent;
 
 public class Inventory {
-
+	private int components[] = {100,101,110,111,200,210,200,301,400,401};
+	int[] quantity = new int[10];
+	int[] numberOfPcs = new int[16];
+	public void updateQuantity(int cid, int q) {
+		quantity[SCM_Agent.getIndex(cid)] += q;
+	}
+	public void updateNumberOfPcs(int sku, int q) {
+		numberOfPcs[sku-1] += q;
+	}
 }
